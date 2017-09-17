664fbf1d520f0ee30fc5f5bab0aac99f37c646b7
cloudartisan <david@cloudartisan.com>
2017-09-17 21:30:09 +1000

    Tweaks scale factor

--- face_recognition_streaming.py
+++ face_recognition_streaming.py
@@ -11,7 +11,7 @@ while cv2.waitKey(1) & 0xFF != ord('q'):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(
         gray,
-        scaleFactor=1.1,
+        scaleFactor=1.2,
         minNeighbors=5,
         minSize=(30, 30),
     )
