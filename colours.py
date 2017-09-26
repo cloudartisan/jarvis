d8f602fcba3696d2669c344f016215d76d9b537f
cloudartisan <david@cloudartisan.com>
2017-09-26 21:44:19 +1000

    Adds HSV values

--- colours.py
+++ colours.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python
 
 
-BGR_WHITE = (255, 255, 255)
-BGR_BLACK = (0, 0, 0)
-BGR_RED = (0, 0, 255)
-BGR_YELLOW = (0, 255, 255)
-BGR_GREEN = (0, 255, 0)
-BGR_BLUE = (255, 0, 0)
+# BGR shorthands for common colours
+WHITE_BGR = (255, 255, 255)
+BLACK_BGR = (0, 0, 0)
+RED_BGR = (0, 0, 255)
+YELLOW_BGR = (0, 255, 255)
+GREEN_BGR = (0, 255, 0)
+BLUE_BGR = (255, 0, 0)
+
+
+# HSV min/max values for the green range
+GREEN_H_MIN = 42
+GREEN_H_MAX = 92
+GREEN_S_MIN = 62
+GREEN_S_MAX = 255
+GREEN_V_MIN = 63
+GREEN_V_MAX = 235
+
+
+# HSV min/max values for the red range
+RED_H_MIN = 0
+RED_H_MAX = 179
+RED_S_MIN = 131
+RED_S_MAX = 255
+RED_V_MIN = 126
+RED_V_MAX = 255
